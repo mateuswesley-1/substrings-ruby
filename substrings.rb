1b2ca4d4cac2757ff353c35995f1b3c269ee8ae7
Mateus Wesley <mateuswesley314@gmail.com>
2022-12-18 17:56:54 -0300

    Create substrings.rb

---
+++ substrings.rb
@@ -0,0 +1,18 @@
+## Done with Ruby 3.1.2
+require 'pry-byebug'
+
+
+# receives a string and a array of sub-strings, and generates a Hash with
+# the substring as keys, and the number os occurrences of the substring in the string as
+# values
+def substring(string, dictionary)
+  sub_counts = Hash.new(0)
+
+  dictionary.each do |substring|
+      sub_counts[substring] += string.scan(/(?=#{substring})/).length
+  end
+  sub_counts
+end
+
+p substring('anamataana xanabelaxana fuceta etanoletaenergetico', ['xana', 'ana', 'fo', 'eta'])
+
